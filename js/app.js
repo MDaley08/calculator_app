e3e905b402e3920ad1285a1794fba2e9cea8f985
Michael Daley <mdaley08@outlook.com>
2024-03-31 08:35:09 -0400

    completed stringParse function that creates stack from string input

--- js/app.js
+++ js/app.js
@@ -1,35 +1,77 @@
+//queries all buttons on page. all buttons are currently only calculator buttons
+const buttonsList = document.querySelectorAll(".calc-btn");
 
-const add = (num1, num2) => {
+
+
+
+
+
+
+
+
+function add(num1, num2){
     return num1 + num2;
 }
-
-const subtract = (num1, num2) => {
+function subtract(num1, num2){
     return num1 - num2;
 }
 
-const multiply = (num1, num2) => {
+function multiply(num1, num2){
     return num1 * num2;
 }
 
-const divide = (numerator, denominator) => {
+function divide(numerator, denominator){
     if(denominator === 0) return NaN;
     return numerator / denominator;
 }
-/**
- * performs addition, subtraction, multiplication or division based on input operator
- * @param {string} operator  string representing +, -, *, or / operator
- * @param {number} num1 
- * @param {number} num2 
- */
-const operate = (operator, num1, num2) => {
-    switch(operator){
-        case '+':
-            return add(num1,num2);
-        case '-':
-            return subtract(num1,num2);
-        case '*':
-            return multiply(num1,num2);
-        case '/':
-            return divide(num1,num2);
+//stingParser lacks character based safety checks due to stings being generated from buttoms that don't allow for unexpected inputs
+function stringParser(string){
+    let numbers = [];
+    let operators = [];
+    let subString = '';
+    let chars = string.split('');
+    chars.forEach(element => {
+        if(element === '+'||element === '-'||element === '*'||element === '/'){
+            operators.push(element);
+            numbers.push(subString);
+            subString = '';
+        }
+        else{
+            subString += element;
+        }
+
+    });
+    numbers.push(subString);
+    return [numbers , operators];
+}
+
+function evaluate(numbers, operators){
+    let numArr = numbers;
+    let oppArr = operators;
+    console.log(numArr);
+
+    while(numArr.length > 1) {
+        let inputNum1;
+        let inputNum2;
+        let operator;
+        inputNum1 = parseInt(numArr.shift());
+        inputNum2 = parseInt(numArr.shift());
+        operator = oppArr.shift();
+
+        switch(operator) {
+            case '+':
+                numArr.unshift(add(inputNum1,inputNum2));
+                break;
+            case '-':
+                numArr.unshift(subtract(inputNum1,inputNum2));
+                break;
+            case '*':
+                numArr.unshift(multiply(inputNum1,inputNum2));
+                break;
+            case '/':
+                numArr.unshift(multiply(inputNum1,inputNum2));
+                break;
+        }
     }
+    return numArr[0];
 }
