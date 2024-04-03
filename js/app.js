8716bded1e5aa9a97a990b3faa6b708e9fd79ef0
Michael Daley <mdaley08@outlook.com>
2024-04-03 03:35:13 -0400

    added updateDisplay, clearDisplay and evaluateExpression functions

--- js/app.js
+++ js/app.js
@@ -1,12 +1,53 @@
 //queries all buttons on page. all buttons are currently only calculator buttons
-const buttonsList = document.querySelectorAll(".calc-btn");
+const clearBtn = document.getElementById("clear");
+const delBtn = document.getElementById("delete");
+const equalBtn = document.getElementById("equal");
+const numpadBtns = document.querySelectorAll(".numpad button");
+const operatorsList = ['+','-','*','/'];
+
+let expression = '0';
+let updated = false;
+let resultShown = false;
+let prevBtn;
+display.textContent = expression;
+
+let updateDisplay = value => {
+    if(!updated||resultShown){
+        display.textContent = value;
+        expression = value;
+        updated = true;
+        resultShown = false;
+        return;
+    } 
+    expression += value;
+    display.textContent = expression;
+}
+
+let evaluateExpression = () => {
+    expression = evaluate(expression);
+    display.textContent = expression;
+    resultShown = true;
+}
+
+let clearDisplay = () =>{
+    display.textContent = '0';
+    expression = '0';
+    updated = false;
+    resultShown = false;
+}
 
 
 
+clearBtn.addEventListener("click", clearDisplay);
+equalBtn.addEventListener("click", evaluateExpression);
 
+console.log(numpadBtns);
 
-
-
+numpadBtns.forEach(element => element.addEventListener("click", () => {
+    if(element.id !== 'equal'){
+        updateDisplay(element.textContent);
+    }
+}));
 
 
 function add(num1, num2){
@@ -24,12 +65,14 @@ function divide(numerator, denominator){
     if(denominator === 0) return NaN;
     return numerator / denominator;
 }
+
 //stingParser lacks character based safety checks due to stings being generated from buttoms that don't allow for unexpected inputs
 function stringParser(string){
     let numbers = [];
     let operators = [];
     let subString = '';
     let chars = string.split('');
+
     chars.forEach(element => {
         if(element === '+'||element === '-'||element === '*'||element === '/'){
             operators.push(element);
@@ -41,21 +84,22 @@ function stringParser(string){
         }
 
     });
+
     numbers.push(subString);
     return [numbers , operators];
 }
 
-function evaluate(numbers, operators){
-    let numArr = numbers;
-    let oppArr = operators;
-    console.log(numArr);
+function evaluate(inString){
+    let tempArr = stringParser(inString);
+    let numArr = tempArr[0];
+    let oppArr = tempArr[1];
 
     while(numArr.length > 1) {
         let inputNum1;
         let inputNum2;
         let operator;
-        inputNum1 = parseInt(numArr.shift());
-        inputNum2 = parseInt(numArr.shift());
+        inputNum1 = parseFloat(numArr.shift());
+        inputNum2 = parseFloat  (numArr.shift());
         operator = oppArr.shift();
 
         switch(operator) {
@@ -69,9 +113,9 @@ function evaluate(numbers, operators){
                 numArr.unshift(multiply(inputNum1,inputNum2));
                 break;
             case '/':
-                numArr.unshift(multiply(inputNum1,inputNum2));
+                numArr.unshift(divide(inputNum1,inputNum2));
                 break;
         }
     }
-    return numArr[0];
+    return numArr[0] || 'Undefined';
 }
